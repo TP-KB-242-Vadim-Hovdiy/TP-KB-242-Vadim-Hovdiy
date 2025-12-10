0af86970cbe1787aad52bd09c275b1b86b76a5e3
TP-KB-242-Vadim-Hovdiy <vadimhovdij@gmail.com>
2025-12-10 16:34:16 +0200

    Class Students

---
+++ topic_07/task1.py
@@ -0,0 +1,19 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def __str__(self):
+        return f" {self.name} , {self.age} років"
+students = [
+Student("Zak", 20),
+Student("Anna", 22),
+Student("John", 19),
+Student("Maria", 21),
+Student("Bob", 23),
+Student("Vadim", 19) 
+]
+
+sorted_students = sorted(students, key=lambda s: s.age)
+print("Відсортований список студентів за віком:")
+for student in sorted_students:
+    print(student)
